01ddd653123c522c04d0ec91c46d17d5dd53b402
Angel Torres <alas4224@gmail.com>
2019-02-27 17:43:56 -0600

    set up database using migrations and knex

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const server = require('./server');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+server.get('/', (req, res) => {
+    res.send('Hello from server!')
+})
+
+server.post('/api/register', (req, res) => {
+
+});
+
+server.post('/api/login', (req, res) => {
+
+});
+
+server.get('/api/users', (req, res) => {
+
+});
 
 server.listen(5000, () => {
     console.log(' \n *** Listening on port 5000 *** \n')
